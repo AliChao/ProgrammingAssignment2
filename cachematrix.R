90b497d1697ee44aaa5273b37589a677112bf2e0
AliChao <alichao78@gmail.com>
2014-05-20 11:42:08 +0800

    AS2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Wthis functionThe first function, makeCacheMatrix creates a special "vector", 
+##which is really a list containing a function to
+##1.set the value of the vector
+##2.get the value of the vector
+##3.set the value of the solve
+##4.get the value of the solve
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) inv <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+    inv <- x$getsolve()
+    if(!is.null(inv)) {
+      message("getting cached inverse matrix")
+      return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setsolve(inv)
+    inv
+  }
         ## Return a matrix that is the inverse of 'x'
-}
